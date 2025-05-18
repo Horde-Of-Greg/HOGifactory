6863273ea0839e4c6e609ef274ae3d1f02d82a53
Alessian Dormal <alessian@alessian.be>
2025-05-18 15:27:21 +0200

    Refactor Tools (#34)

--- tools/manifest/makeReadableManifest.js
+++ tools/manifest/makeReadableManifest.js
@@ -1,26 +1,28 @@
 import originalManifest from "../../manifest.json" assert { type: "json" };
 import fs from "fs";
 import path from "path";
-import { CurseForgeUtil } from "../util/curseforge.js";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import { GlobalUtil } from "../util/global.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-async function updateManifest() {
-  const jobs = originalManifest.files.map(async (rawMod) => {
-    const json = await CurseForgeUtil.makeFileRequest(rawMod);
-    rawMod.name = json.data.fileName;
-  });
-  await Promise.all(jobs);
+(async () => {
+  await Promise.all(
+    originalManifest.files.map(async (modEntry) => {
+      modEntry.name = await GlobalUtil.formatModNameVer(
+        modEntry.projectID,
+        modEntry.fileID
+      );
+    })
+  );
 
   fs.writeFileSync(
     path.join(__dirname, "../templates/manifest.json"),
     JSON.stringify(originalManifest, null, 2),
     "utf-8"
   );
-}
+})();
 
-updateManifest();
 console.log("Manifest updated");
