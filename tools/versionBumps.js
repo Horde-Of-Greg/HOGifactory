61be4c0dc7d1a78767e2d34aa1063e2c129a06a5
D-Alessian <alessian@alessian.be>
2025-06-02 13:26:53 +0200

    Just refactoring the version bump logic to avoid a possible scenario where github would give a bump on every commit

---
+++ tools/versionBumps.js
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const args = process.argv.slice(2);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const templateManifest = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "./templates/manifest.json"), "utf-8")
+);
+
+//switch (args[0])
+    //case manifest
+        const version = fs.readFileSync(path.join(__dirname, "../.manifest-version-lock.txt")).toString();
+        const newVersion = version.split(".").fill((parseInt(version.split(".")[1]) + 1).toString(), 1, 2).join(".");
+
+        templateManifest.version = newVersion
+
+        fs.writeFileSync(
+            path.join(__dirname, "../manifest.json"),
+            JSON.stringify(templateManifest, null, 2),
+            "utf-8"
+        );
+
+        fs.writeFileSync(
+            path.join(__dirname, "../.manifest-version-lock.txt"),
+            newVersion,
+            "utf8"
+        );
+        // break;
