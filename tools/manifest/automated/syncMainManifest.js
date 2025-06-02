ca77c3b6fc89e5fa16131a8c326c176cc5e5205d
D-Alessian <alessian@alessian.be>
2025-06-02 12:53:52 +0200

    Revert manifest.json to original state for actions, and add auto versioning to it

--- tools/manifest/automated/syncMainManifest.js
+++ tools/manifest/automated/syncMainManifest.js
@@ -8,13 +8,22 @@ const __dirname = dirname(__filename);
 const template = JSON.parse(
   fs.readFileSync(path.join(__dirname, "../../templates/manifest.json"), "utf-8")
 );
+const version = fs.readFileSync(path.join(__dirname, "../../../.manifest-version-lock.txt")).toString();
+const newVersion = version.split(".").fill((parseInt(version.split(".")[1]) + 1).toString(), 1, 2).join(".");
 
 template.files = template.files.map(({ name, ...rest }) => rest);
+template.version = newVersion;
 
 fs.writeFileSync(
-  path.join(__dirname, "../../../manifest.json"),
-  JSON.stringify(template, null, 2),
-  "utf-8"
+    path.join(__dirname, "../../../test-manifest.json"),
+    JSON.stringify(template, null, 2),
+    "utf-8"
+);
+
+fs.writeFileSync(
+    path.join(__dirname, "../../../.manifest-version-lock.txt"),
+    newVersion,
+    "utf8"
 );
 
 console.log("Manifest synced");
